fb28f28c697787c0b878ec31a575bebac3bb48c1
Hartmut Schorrig <hartmut@vishia.org>
2012-03-26 00:00:00 +0200

    * +javacjarbase.sh now tested with compilation of srcJava_ZBNF (other module), renamed   * makejar_zbnf.sh now uses +javacjarbase.sh and combiles to zbnfjax/zbnf.jar with same capabilities as in the past, contains necessary classes for ZBNF only.

--- _make/makejar_zbnf.sh
+++ _make/makejar_zbnf.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
+##This script compiles Java files to build the zbnfjax/zbnf.jar. This file contains all files to run the ZBNF parser and all zbnfjax capabilities.
+##It contains that sources from the srcJava_vishiaBase component which were need to run this.
+##It is not the compilation of all capabilities of vishiaBase.
 
-## The java-copiler may be located at a user-specified position.
-## Set the environment variable JAVA_HOME, where bin/javac will be found.
-if test "$JAVA_JDK" = "";  then export JAVA_JDK="/usr/share/JDK"; fi
-#set PATH="$JAVA_JDK_HOME\bin:$PATH"
 
 ## The TMP_JAVAC is a directory, which contains only this compiling results. It will be clean in the batch processing.
 export TMP_JAVAC="../../../tmp_javac"
 
 ## Output jar-file with path and filename relative from current dir:
-export OUTPUTFILE_JAVAC="../../exe/zbnf.jar"
-export OUTPUTDIR_JAVAC="../../exe/zbnf.jar"
+export OUTDIR_JAVAC="../../zbnfjax"
+export JAR_JAVAC="zbnf.jar"
 
 ## Manifest-file for jar building relativ path from current dir:
 export MANIFEST_JAVAC="zbnf.manifest"
@@ -26,9 +25,9 @@ export INPUT_JAVAC
 export CLASSPATH_JAVAC="nothing"
 
 ## Sets the src-path for further necessary sources:
-export SRCPATH_JAVAC=".."
+export SRCPATH_JAVAC="..:../../srcJava_vishiaBase"
 
 ## Call java-compilation and jar within zbnfjax with given input environment:
 #zbnfjax javacjar
-./javacjar.sh
+../../srcJava_vishiaBase/_make/+javacjarbase.sh
 
