464e165bae4a8b6204ab841581babcc88e843d45
Hartmut Schorrig <hartmut@vishia.org>
2012-04-12 00:00:00 +0200

    compile jar in linux

--- _make/makejar_zbnf.sh
+++ _make/makejar_zbnf.sh
@@ -8,7 +8,7 @@
 export TMP_JAVAC="../../../tmp_javac"
 
 ## Output jar-file with path and filename relative from current dir:
-export OUTDIR_JAVAC="../../zbnfjax"
+export OUTDIR_JAVAC="../../exe"
 export JAR_JAVAC="zbnf.jar"
 
 ## Manifest-file for jar building relativ path from current dir:
