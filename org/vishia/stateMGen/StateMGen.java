f9adfc9bf0e31f2d60cb31a5393a39012fff22fc
Hartmut Schorrig <hartmut@vishia.org>
2014-07-24 00:00:00 +0200

    * JZcmdSyntax: comments in text <&-comment> <:->comment<.->   * StateMGen: scriptVariable is stateData instead state, better more non-ambigous.

--- org/vishia/stateMGen/StateMGen.java
+++ org/vishia/stateMGen/StateMGen.java
@@ -23,9 +23,49 @@ import org.vishia.zcmd.JZcmd;
 import org.vishia.zcmd.Zbnf2Text;
 import org.vishia.zcmd.Zbnf2Text.Out;
 
-
+/**This class prepares information for a state machine from representation in text format 
+ * for generation C-code and documentation.
+ * It calls the Zbnf parser and Zbnf2Text for generation.
+ * See {@link #main(String[])}
+ * 
+ * @author Hartmut Schorrig
+ *
+ */
 public class StateMGen {
   
+  /**Version, history and license.
+   * <ul>
+   * <li>2014-06-15 Hartmut chg: scriptVariable is stateData instead state, better more non-ambiguous.
+   * <li>
+   * <li>2012-10-07 Hartmut creation 
+   * </ul>
+   * 
+   * <b>Copyright/Copyleft</b>:
+   * For this source the LGPL Lesser General Public License, published by the Free Software Foundation is valid.
+   * It means:
+   * <ol>
+   * <li> You can use this source without any restriction for any desired purpose.
+   * <li> You can redistribute copies of this source to everybody.
+   * <li> Every user of this source, also the user of redistribute copies
+   *    with or without payment, must accept this license for further using.
+   * <li> But the LPGL is not appropriate for a whole software product,
+   *    if this source is only a part of them. It means, the user
+   *    must publish this part of source,
+   *    but don't need to publish the whole source of the own product.
+   * <li> You can study and modify (improve) this source
+   *    for own using or for redistribution, but you have to license the
+   *    modified sources likewise under this LGPL Lesser General Public License.
+   *    You mustn't delete this Copyright/Copyleft inscription in this source file.
+   * </ol>
+   * If you are intent to use this sources without publishing its usage, you can get
+   * a second license subscribing a special contract with the author. 
+   * 
+   * @author Hartmut Schorrig = hartmut.schorrig@vishia.de
+   * 
+   * 
+   */
+  //@SuppressWarnings("hiding")
+  static final public String sVersion = "2014-06-26";
 
 
   /** Aggregation to the Console implementation class.*/
@@ -524,7 +564,7 @@ public class StateMGen {
           outData.append("===================").append(outArgs.sFileScript);
         }
         Writer out = new FileWriter(fOut);
-        generator.setScriptVariable("state", 'O', stateData, true);
+        generator.setScriptVariable("stateData", 'O', stateData, true);
         try{ 
           JZcmd.execute(generator, fileScript, out, console.currdir(), true, args.sScriptCheck == null ? null : new File(args.sScriptCheck), console);
           console.writeInfoln("SUCCESS outfile: " + fOut.getAbsolutePath());
