08c4a3a4c962ee07c97a6fdaeff317c674d1a015
Hartmut Schorrig <hartmut@vishia.org>
2015-03-08 00:00:00 +0100

    improvements

--- org/vishia/zcmd/Csv2Data.java
+++ org/vishia/zcmd/Csv2Data.java
@@ -5,7 +5,6 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.text.ParseException;
@@ -13,18 +12,13 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-import java.util.TreeMap;
 
-import org.vishia.cmd.JZcmdExecuter;
-import org.vishia.cmd.JZcmdScript;
 import org.vishia.mainCmd.MainCmd;
 import org.vishia.mainCmd.MainCmdLogging_ifc;
-import org.vishia.mainCmd.MainCmd_ifc;
 import org.vishia.util.Assert;
 import org.vishia.util.FileSystem;
 import org.vishia.util.IndexMultiTable;
 import org.vishia.util.StringFunctions;
-import org.vishia.util.StringPart;
 import org.vishia.util.StringPart;
 import org.vishia.util.StringPartFromFile;
 import org.vishia.util.StringPartScan;
@@ -103,6 +97,14 @@ public class Csv2Data
   /**Column where the identifier is found. */
   int colident;
   
+  /**The character which is the separator of columns in the line.
+   * It is either ";" or ",", detect from the first line in {@link #createColumns(String)}. 
+   */
+  char separator;
+  
+  /**'.' if the {@link #separator} is ',', elsewhere ','. */
+  char cDecimalSep;
+  
   public Csv2Data(MainCmdLogging_ifc log){
     this.log = log;
   }
@@ -159,9 +161,24 @@ public class Csv2Data
   }
   
   
+  /**The first line should contain the identifier for the columns.
+   * The column of that identifier which is named "Identifier" is set as {@link #colident}.
+   * It is the column which contains the identifier for the line.
+   * @param sLine
+   */
   public void createColumns(String sLine){
     columns.clear();
-    String[] aColumns = sLine.split(";");
+    String[] aColumns = sLine.split(";");   
+    String[] aColumns2 = sLine.split(",");
+    //detect separator by testing the proper splitting char:
+    if(aColumns.length > aColumns2.length){  
+      separator = ';';
+      cDecimalSep = ',';
+    } else {
+      separator = ',';
+      cDecimalSep = '.';
+      aColumns = aColumns2;
+    }
     int col = 0;
     for(String sColumn1: aColumns){
       String sColumn = sColumn1.trim();
@@ -208,7 +225,7 @@ public class Csv2Data
     String sIdent = null;
     while(cont){
       posQuotion = sLine.indexOf('\"', pos);
-      posColon = sLine.indexOf(';', pos);
+      posColon = sLine.indexOf(separator, pos);
       String sCell;
       if(posColon <0){ 
         posColon = sLine.length(); 
@@ -281,7 +298,7 @@ public class Csv2Data
     try{
       if(cell.indexOf(',')<0)
         Assert.stop();
-      float val = StringFunctions.parseFloat(cell, 0, Integer.MAX_VALUE, ',', null);
+      float val = StringFunctions.parseFloat(cell, 0, Integer.MAX_VALUE, cDecimalSep, null);
       return val;
     } catch(Exception exc){
       return 777777.7f;
