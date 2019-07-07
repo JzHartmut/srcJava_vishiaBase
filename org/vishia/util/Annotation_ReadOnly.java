56d07a0290f72474895d65d7779b67d4b571533a
Hartmut Schorrig <hartmut.schorrig@vishia.de>
2019-07-07 10:29:37 +0200

    Annotation @ReadOnly only for experience, not disturbing

---
+++ org/vishia/util/Annotation_ReadOnly.java
@@ -0,0 +1,12 @@
+package org.vishia.util;
+
+//import java.lang.annotation.Retention;
+//import java.lang.annotation.RetentionPolicy;
+
+
+public class Annotation_ReadOnly
+{
+
+  
+  public @interface ReadOnly{}
+}
