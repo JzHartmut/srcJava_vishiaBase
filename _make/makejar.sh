46bd39e845517cf1be5f154475a06a3d8081c852
Hartmut Schorrig <hartmut@vishia.org>
2020-03-21 18:36:14 +0100

    Some changes in scripts, no change content.

--- _make/makejar.sh
+++ _make/makejar.sh
@@ -2,33 +2,29 @@
 #it can be run under Windows using MinGW: sh.exe - thisScript.sh
 #MinGW is part of git, it should be known for Gcc compile too.
 
-##This should be correct for any new version, used for gradle build and for shell build!
+##Both variables should be corrected for any new version, 
+##if is used for gradle build and for shell build!
 if test "$VERSION" == ""; then export VERSION="2020-03-20"; fi
-if test "$TIME" == ""; then export TIME="2020-03-20+06:11"; fi
+export TIME="2020-03-20+06:11"
 
+#determine out file names from VERSION
 export JARFILE=$DEPLOY$VERSION.jar
 export MD5FILE=$DEPLOY$VERSION.jar.MD5.txt
 
 echo compile java and generate jar with binary-compatible content. 
-#echo Java-Tools at $JAVAC_HOME
-#echo time stamp and version = $VERSION
-#echo primary sources to compile in file $PRIMARYSRCFILE
-#echo sourcepath: $SRCPATH
-#echo classpath : $CLASSPATH
-#echo temparary files at $TMPJAVAC
-#echo jar-file to generate = $JARFILE
-#echo MD5-file to generate = $MD5FILE
-#it is better to read:
 echo JAVAC_HOME = $JAVAC_HOME
-echo VERSION = $VERSION
-echo TIME = $TIME
-echo FILE1SRC = $FILE1SRC
-echo SRCPATH = $SRCPATH
+echo VERSION = $VERSION  - determine only output file names
+echo TIME = $TIME  - determine timestamp in jar
+echo SRC_ALL = $SRC_ALL  - gather all *.java there
+echo SRC_ALL2 = $SRC_ALL2  - gather all *.java there
+echo FILE1SRC = $FILE1SRC  - argument files for javac
+echo RESOURCEFILES = $RESOURCEFILES  - additional files in jar
+echo SRCPATH = $SRCPATH  - search path sources for javac
 echo CLASSPATH = $CLASSPATH
-echo VishiaBASE = $VISHIABASE
-echo TMPJAVAC =  $TMPJAVAC
-echo JARFILE = $JARFILE
-echo MD5FILE = $MD5FILE
+echo JAR_vishiaBASE = $JAR_vishiaBase  - jar file for jar generation
+echo TMPJAVAC =  $TMPJAVAC  - temporary files while compilation
+echo JARFILE = $JARFILE  - generated jar
+echo MD5FILE = $MD5FILE  - generated MD5 text file
 
 if test "$JAVAC_HOME" = ""; then
   echo you must set JAVAC_HOME in your system to the installed JDK
@@ -39,20 +35,22 @@ if test -d $TMPJAVAC/binjar; then rm -f -r -d $TMPJAVAC/binjar; fi
 mkdir -p $TMPJAVAC/binjar
 
 if ! test "$SRC_ALL" = ""; then
-  echo gather all sources, at $SRC_ALL
+  echo source-set all files = $SRC_ALL
   find $SRC_ALL -name "*.java" > $TMPJAVAC/sources.txt
   export FILE1SRC=@$TMPJAVAC/sources.txt
 fi  
+if ! test "$SRC_ALL2" = ""; then
+  echo source-set all files = $SRC_ALL2
+  find $SRC_ALL2 -name "*.java" >> $TMPJAVAC/sources.txt
+  export FILE1SRC=@$TMPJAVAC/sources.txt
+fi  
 echo compile javac
+echo $JAVAC_HOME/bin/javac -encoding UTF-8 -d $TMPJAVAC/binjar -cp $CLASSPATH -sourcepath $SRCPATH $FILE1SRC 
 $JAVAC_HOME/bin/javac -encoding UTF-8 -d $TMPJAVAC/binjar -cp $CLASSPATH -sourcepath $SRCPATH $FILE1SRC 
-##mkdir $TMPJAVAC/binjar/META-INF
-##Note: create the manifest file manually, not with jar, because of time stamp
-##cp $MANIFEST $TMPJAVAC/binjar/META-INF/MANIFEST.MF  
-##echo touch timestams to $VERSION
-##find $TMPJAVAC/binjar -exec touch -d $VERSION {} \;
+
 echo build jar
-##Hint: Use the new generated class files itself, it contains the necessary code:
-java -cp $VISHIABASE org.vishia.util.Zip -o:$JARFILE -manifest:$MANIFEST -sort -time:$TIME $TMPJAVAC/binjar:**/*.class
+echo java -cp $JAR_vishiaBase org.vishia.util.Zip -o:$JARFILE -manifest:$MANIFEST -sort -time:$TIME  $TMPJAVAC/binjar:**/*.class $RESOURCEFILES
+java -cp $JAR_vishiaBase org.vishia.util.Zip -o:$JARFILE -manifest:$MANIFEST -sort -time:$TIME  $TMPJAVAC/binjar:**/*.class $RESOURCEFILES
 ##$JAVAC_HOME/bin/jar -n0cvfM $JARFILE -C $TMPJAVAC/binjar . > $TMPJAVAC/jar.txt
 if ! test "$MD5FILE" = ""; then echo output MD5 checksum
   md5sum -b $JARFILE > $MD5FILE
--- _make/makejar_vishiaBase.sh
+++ _make/makejar_vishiaBase.sh
@@ -2,30 +2,28 @@
 #edit some settings if there are different form default.
 
 export TMPJAVAC=/tmp/javac_vishiaBase/build/javac
+
 #export JAVAC_HOME=c:/Programs/Java/jdk1.8.0_211
 #export JAVAC_HOME=c:/Programs/Java/jdk1.8.0_241
 export JAVAC_HOME=/usr/share/JDK/jdk1.8.0_241
 
+#Output files
+export DEPLOY=../vishiaBase-
+
 #It determines only the file names, the TIME is inside makejar.sh
-export VERSION="2020-03-20"
+#export VERSION="2020-03-20"
 
 #use the yet compiled class to generate jar
-export VISHIABASE=$TMPJAVAC/binjar
-# SWT for Windows-64
-##export CLASSPATH=../../../libs/org.eclipse.swt.win32.win32.x86_64_3.110.0.v20190305-0602.jar;../../../../cmpnJava_vishiaBase/libs/vishiaBase-2020-03-14.jar
+export JAR_vishiaBase=$TMPJAVAC/binjar
 
 export CLASSPATH=xx
 # located from this workingdir as currdir for shell execution:
 export SRCPATH=..
 export MANIFEST=vishiaBase.manifest
-# FILE1SRC=../org/vishia/jztxtcmd/JZtxtcmd.java
 export SRC_ALL=..
 
-#Output files
-export DEPLOY=../vishiaBase-
-
-chmod 777 makejar.sh
 #now run the common script:
+chmod 777 makejar.sh
 ./makejar.sh
 
 
