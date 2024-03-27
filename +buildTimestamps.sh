9c7d99446291aedb85f8d108c55e260115e7ebef
Hartmut Schorrig <hartmut.schorrig@vishia.org>
2024-03-27 14:14:21 +0100

    +buildTimestamps.sh uses ../../tools/vishiaBase.jar should be work always.

--- +buildTimestamps.sh
+++ +buildTimestamps.sh
@@ -1,2 +1,3 @@
 echo build the timestamp in .filelist from all files:
-~/cmds/javavishia.sh org.vishia.util.FileList L -l:.filelist -d:. "-m:[**/[~#*|~*#]|~_*]"
+java -cp ../../tools/vishiaBase.jar org.vishia.util.FileList L -l:.filelist -d:. "-m:[**/[~#*|~*#]|~_*]"
+##~/cmds/javavishia.sh org.vishia.util.FileList L -l:.filelist -d:. "-m:[**/[~#*|~*#]|~_*]"
