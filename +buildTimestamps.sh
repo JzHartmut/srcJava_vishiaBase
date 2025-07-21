4592a63aab010816f87fd963d368e18f0eb6564c
Hartmut Schorrig <hartmut.schorrig@vishia.de>
2025-07-21 21:29:37 +0200

    fixed buildtimestamp, other environment. jars should always present

--- +buildTimestamps.sh
+++ +buildTimestamps.sh
@@ -1,3 +1,3 @@
 echo build the timestamp in .filelist from all files:
-java -cp ../../tools/vishiaBase.jar org.vishia.util.FileList L -l:.filelist -d:. "-m:[**/[~#*|~*#]|~_*]"
+java -cp ../../jars/vishiaBase.jar org.vishia.util.FileList L -l:.filelist -d:. "-m:[**/[~#*|~*#]|~_*]"
 ##~/cmds/javavishia.sh org.vishia.util.FileList L -l:.filelist -d:. "-m:[**/[~#*|~*#]|~_*]"
